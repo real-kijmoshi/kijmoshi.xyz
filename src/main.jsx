962b29cc248fa6f706b30726d9acd962d718179f
kijmoshi <94071547+real-kijmoshi@users.noreply.github.com>
2023-08-23 21:32:31 +0100

    quick fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
